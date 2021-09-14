22a0daf4dd9558ad22cdf710633ec5e732c5c5aa
Luuk Gille <lgille@competa.com>
2021-09-14 10:53:37 +0200

    initial commit

---
+++ src/components/WeatherAPI/index.js
@@ -0,0 +1,11 @@
+import styles from './weather.module.scss'
+
+function WeatherAPI() {
+    return (
+        <>
+            <div className={styles.weatherContainer}>Welcome to my weather app</div>
+        </>
+    )
+  }
+
+export default WeatherAPI;
