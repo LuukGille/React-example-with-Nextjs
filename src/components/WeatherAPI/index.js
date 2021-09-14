df7c5724cc5df3bfacf2059c9853e1f71e29c424
Luuk Gille <lgille@competa.com>
2021-09-14 14:25:58 +0200

    creating a weather API to display country and temperature (WIP)

--- src/components/WeatherAPI/index.js
+++ src/components/WeatherAPI/index.js
@@ -1,9 +1,35 @@
-import styles from './weather.module.scss'
+import React, { useState, useEffect } from 'react';
+import styles from './weather.module.scss';
+import axios from 'axios';
 
-function WeatherAPI() {
+const WeatherAPI = ()  => {
+
+  const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async (props) => {
+      const result = await axios(
+        `https://api.openweathermap.org/data/2.5/weather?q=rotterdam&units=metric&appid=6efe7a3954e41d023b4a18ddfd64c421`,
+      );
+      setData([result.data]);
+    };
+    fetchData();
+  }, []);
+
     return (
         <>
-            <div className={styles.weatherContainer}>Welcome to my weather app</div>
+            <div className={styles.weatherContainer}>
+                <div className={styles.weatherSection}>
+                    <h1 className={styles.weatherSectionTitle}>What is the temperature?</h1>
+                    {data.map((item) => (
+                      <div key={item}>
+                        <div>{item.name}</div>
+                        <div>{item.main.temp}</div>
+                      </div>
+                    ))}
+                </div> 
+            </div>
         </>
     )
   }
