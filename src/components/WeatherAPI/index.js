4170fa78dcd0346b1a8f7d2a7f5a4c8feca3e94a
Luuk Gille <lgille@competa.com>
2021-09-14 16:53:49 +0200

    created error handeling and input field for cities

--- src/components/WeatherAPI/index.js
+++ src/components/WeatherAPI/index.js
@@ -4,33 +4,38 @@ import axios from 'axios';
 
 const WeatherAPI = ()  => {
 
-
+  let [city, setCity] = useState('rotterdam');
   const [data, setData] = useState([]);
   useEffect(() => {
     const API_KEY = process.env.apiWeatherKey;
 
-    const fetchData = async (props) => {
+    const fetchData = async () => {
       const result = await axios(
-        `https://api.openweathermap.org/data/2.5/weather?q=rotterdam&units=metric&appid=${API_KEY}`
-      );
-      setData([result.data]);
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+        )    .then(result => {
+            setData([result.data]);
+        })    .catch(err => {
+          console.log(err)
+      });
     };
     fetchData();
-  }, []);
+  }, [city]);
 
     return (
         <>
-            <div className={styles.weatherContainer}>
-                <div className={styles.weatherSection}>
-                    <h1 className={styles.weatherSectionTitle}>What is the temperature?</h1>
-                    {data.map((item) => (
-                      <div key={item}>
-                        <div>{item.name}</div>
-                        <div>{item.main.temp}</div>
-                      </div>
-                    ))}
-                </div> 
-            </div>
+          <div className={styles.weatherContainer}>
+              <div className={styles.weatherSection}>
+                  <h1 className={styles.weatherSectionTitle}>What is the temperature?</h1>
+                  <p>Fill in your city to see your temperature</p>
+                  <input onChange={(e) => setCity(e.target.value)} />
+                  {data.map((item) => (
+                    <div key={item}>
+                      <div>{item.name}</div>
+                      <div>{item.main.temp}</div>
+                    </div>
+                  ))}
+              </div> 
+          </div>
         </>
     )
   }
